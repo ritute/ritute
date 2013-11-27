6b96ed87f74a92251656ae28aeda8fc91c1ab22c
Rita Bancewicz <rita.bancewicz@gmail.com>
2013-11-26 22:41:24 -0500

    removed reference to twitter account (no longer own that twitter handle)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ritute::Application.config.session_store :cookie_store, key: '_ritute_session'
+Ritute::Application.config.session_store :cookie_store, :key => '_ritute_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
